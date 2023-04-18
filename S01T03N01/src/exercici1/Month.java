6a99a355e428af13b7d64fb297a1ca84485529e7
evelynit <130852224+evelynit@users.noreply.github.com>
2023-04-18 16:07:58 +0200

    Add files via upload

---
+++ S01T03N01/src/exercici1/Month.java
@@ -0,0 +1,34 @@
+package exercici1;
+
+import java.util.Objects;
+
+public class Month {
+	private String name;
+	
+	public Month(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Month other = (Month) obj;
+		return Objects.equals(name, other.name);
+	}
+	
+	
+}
